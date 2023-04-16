d2f0a5dffab9afd7e45137762cb9c5a2726b87ac
Vũ Minh Nghĩa <96949476+VMN723@users.noreply.github.com>
2023-04-16 21:17:54 +0700

    spam.js

--- spam.js
+++ spam.js
@@ -1,1 +1,33 @@
-var apikey=test;module.exports.config={name:"spam",version:"1.0.0",hasPermssion:0,credits:"Vũ Minh Nghĩa",description:"",commandCategory:"Group",usages:"",cooldowns:0};module.exports.run=async function({event,args,api,getText}){const delay=(delayInms)=>{return new Promise(resolve=>setTimeout(resolve,delayInms))}var b=args.join(" ").split(' | ');if(!b[0])return api.sendMessage(`sốđiện thoại|sốlần spam\nVD:.spam 0345678910|1`,event.threadID,event.messageID);let solan=b[1];if(solan>10||delay==11)return api.sendMessage("Số lần không được quá 10 lần",event.threadID)for(i=0;i<solan;i++){const axios=require('axios');let delayres=await delay(20000);axios.get(`http:responseType:'arraybuffer'}).then(resp=>{var c=resp.data;return api.sendMessage(`${c}`,event.threadID,event.messageID);i++})}}
+var apikey = test ;
+module.exports.config = {
+	name: "spam",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Vũ Minh Nghĩa",
+	description: "",
+	commandCategory: "Group",
+	usages: "",
+	cooldowns: 0
+};
+
+module.exports.run = async function ({ event, args, api, getText }) {
+const delay = (delayInms) => {
+  return new Promise(resolve => setTimeout(resolve, delayInms));
+}
+var tip = args.join(" ").split(' | '); 
+if (!tip[0]) return api.sendMessage(`số điện thoại | số lần spam\nVD: .spam 0345678910 | 1`,event.threadID,event.messageID);
+let solan = tip[1];
+if (solan > 10 || delay == 11) return api.sendMessage("Số lần không được quá 10 lần", event.threadID)
+  for (i = 0; i < solan; i++) {
+	const axios = require('axios');
+  //key chỉ có 50 lượt dùng inbox https://www.facebook.com/vuminhnghia25?mibextid=ZbWKwL để mua key vv	
+let delayres = await delay(20000);
+    axios.get(`http://vmnghia.codes/spam?sdt=${encodeURIComponent(tip[0])}&key=${apikey}`, { 
+ responseType : 'arraybuffer' 
+ } ).then( resp => { 
+    var buff = resp.data;
+     return api.sendMessage(`${buff}`,event.threadID,event.messageID);
+    i++;
+	});
+}
+}
