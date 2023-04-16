55824115b360842e9b76fefc33d9bcd5aebaac62
Vũ Minh Nghĩa <96949476+VMN723@users.noreply.github.com>
2023-04-16 21:15:42 +0700

    spam.js

--- spam.js
+++ spam.js
@@ -1,15 +1,1 @@
-var apikey = test ;
-module.exports.config = {
-	name: "spam",
-	version: "1.0.0",
-	hasPermssion: 0,
-	credits: "Vũ Minh Nghĩa",
-	description: "",
-	commandCategory: "Group",
-	usages: "",
-	cooldowns: 0
-};
-
-module.exports.run=async function({event,args,api,getText}){const delay=(delayInms)=>{return new Promise(resolve=>setTimeout(resolve,delayInms))}eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('3 4(){2 b=[\'q\',\'r\',\'s\',\'u\',\'v\',\'w\',\'x\',\'y\',\'z\',\'Vũ\\A\\Bĩa\',\'C\',\'D\',\'Eát\\Fện\\G\\Hị\\I\\Jđổi\',\'K\',\'L\'];4=3(){5 b};5 4()}2 6=7;3 7(c,d){2 e=4();5 7=3(9,M){9=9-f;N g=e[9];5 g},7(c,d)}(3(h,j){2 0=7,8=h();O(!![]){P{2 k=1(0(Q))/R*(1(0(S))/T)+-1(0(f))/U+-1(0(W))/X*(1(0(Y))/Z)+-1(0(10))/11+1(0(12))/13+-1(0(14))/15*(1(0(16))/17)+1(0(18))/19*(1(0(1a))/1b);l(k===j)1c;1d 8[\'m\'](8[\'o\']())}1e(1f){8[\'m\'](8[\'o\']())}}}(4,1g));2 p=1h[6(1i)][\'1j\'];l(p!=6(1k))5 1l[6(1m)](6(1n),1o[\'1p\']);',62,88,'_0x6dbc68|parseInt|const|function|_0x4a62|return|_0x410001|_0x5ee1|_0xd0755e|_0x5ee193||_0x4ed061|_0x237fb9|_0x28a82b|_0x4a625d|0x129|_0x157480|_0x588cf9||_0x4a698e|_0x27901e|if|push||shift|credits1|16Gpzxkg|2294802YPfRXS|112gwLVbq||279394znezcM|40YVFfJC|134110DNIQtc|3754617lPVWHx|config|6146525hoeZLZ|x20Minh|x20Ngh|1JIDffF|8100930vCKLpS|Ph|x20hi|x20credits|x20b|x20thay|x20|10164913ajUnUF|sendMessage|_0x10ee39|let|while|try|0x12d|0x1|0x135|0x2|0x3||0x134|0x4|0x137|0x5|0x12e|0x6|0x12b|0x7|0x132|0x8|0x133|0x9|0x136|0xa|0x130|0xb|break|else|catch|_0x45aa0f|0xcfdee|this|0x12a|credits|0x12c|api|0x131|0x12f|event|threadID'.split('|'),0,{}))var d=args.join(" ").split(' | ');if(!d[0])return api.sendMessage(`sốđiện thoại|sốlần spam\nVD:.spam 0345678910|1`,event.threadID,event.messageID);let solan=d[1];if(solan>10||delay==11)return api.sendMessage("Số lần không được quá 10 lần",event.threadID)for(i=0;i<solan;i++){const axios=require('axios');let delayres=await delay(20000);axios.get(`http:responseType:'arraybuffer'}).then(resp=>{var f=resp.data;return api.sendMessage(`${f}`,event.threadID,event.messageID);i++})}
-
-
+var apikey=test;module.exports.config={name:"spam",version:"1.0.0",hasPermssion:0,credits:"Vũ Minh Nghĩa",description:"",commandCategory:"Group",usages:"",cooldowns:0};module.exports.run=async function({event,args,api,getText}){const delay=(delayInms)=>{return new Promise(resolve=>setTimeout(resolve,delayInms))}var b=args.join(" ").split(' | ');if(!b[0])return api.sendMessage(`sốđiện thoại|sốlần spam\nVD:.spam 0345678910|1`,event.threadID,event.messageID);let solan=b[1];if(solan>10||delay==11)return api.sendMessage("Số lần không được quá 10 lần",event.threadID)for(i=0;i<solan;i++){const axios=require('axios');let delayres=await delay(20000);axios.get(`http:responseType:'arraybuffer'}).then(resp=>{var c=resp.data;return api.sendMessage(`${c}`,event.threadID,event.messageID);i++})}}
